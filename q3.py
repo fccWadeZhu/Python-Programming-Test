d5a7920e7bf1759bdb10a6f0c25f59c402ab0dbf
fccWadeZhu <36261197+fccWadeZhu@users.noreply.github.com>
2018-02-10 01:05:32 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -17,20 +17,22 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 import json
-if __name__ == '__main__':
+
+def crawleappsByCaseId(caseId, isGenerateJson):
     result = {}
     try:
-        url = 'https://eapps.courts.state.va.us/cav-public/caseInquiry/showCasePublicInquiry?caseId=23811'
-        values = {"caseId": "23811"}
-        headers = {'Cookie':'JSESSIONID=0000ZRWCvFtTH5Kz2JlSKksfY3v:1962kbr3k','Host':'eapps.courts.state.va.us','User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'}
+        url = 'https://eapps.courts.state.va.us/cav-public/caseInquiry/showCasePublicInquiry?caseId'
+        values = {"caseId": caseId}
+        headers = {'Cookie': 'JSESSIONID=0000ZRWCvFtTH5Kz2JlSKksfY3v:1962kbr3k', 'Host': 'eapps.courts.state.va.us',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'}
         data = urllib.urlencode(values)
-        request = urllib2.Request(url, data,headers)
+        request = urllib2.Request(url, data, headers)
         response = urllib2.urlopen(request)
         webpage = response.read().decode('utf-8')
         soup = BeautifulSoup(webpage, 'html.parser')
         # get Appellant name
-        appellantTable = soup.find('table',id='listofPartiesAPL')
-        appellantTds = appellantTable.find('tr',class_='alternaterow2').find_all('td')
+        appellantTable = soup.find('table', id='listofPartiesAPL')
+        appellantTds = appellantTable.find('tr', class_='alternaterow2').find_all('td')
         appellantName = appellantTds[0].get_text().strip()
         result['Appellant name'] = appellantName
         # get Appellee name
@@ -39,18 +41,25 @@ if __name__ == '__main__':
         appelleeName = appelleeTds[0].get_text().strip()
         result['Appellee name'] = appelleeName
         # get CAV record number
-        cavRecordDiv = soup.find('div',class_='customtab')
-        cavBtag =cavRecordDiv.find('b')
-        cavRecordNumber = cavBtag.get_text().replace('CAV Record #','').strip()
+        cavRecordInput = soup.find('input', id='caseNumber')
+        cavRecordNumber = cavRecordInput['value'].strip()
         result['CAV record number'] = cavRecordNumber
         # get date recieved
-        cavReceiveDiv = soup.find('div',id='noticeOfAppealSlider')
+        cavReceiveDiv = soup.find('div', id='noticeOfAppealSlider')
         cavReceiveTds = cavReceiveDiv.find('table').find_all('td')
         cavReceivedDate = cavReceiveTds[1].find('input')['value']
         result['CAV date recieved'] = cavReceivedDate
-        jsonResult = json.dumps(result)
-        f = open("./q3.json", "w")
-        f.write(jsonResult)
-        f.close()
+        if isGenerateJson:
+
+            jsonResult = json.dumps(result)
+            f = open("./q3.json", "w")
+            f.write(jsonResult)
+            f.close()
+
     except Exception as e:
         print e
+    return result
+
+if __name__ == '__main__':
+    crawleappsByCaseId('23811', True)
+    print 'complete!'
